92f076e36cc4ba1416b3e6b2be97e79ada49a11c
HeHuiqi <1710308677@qq.com>
2021-09-16 11:58:32 +0800

    change prd config

--- webpack.prd.config.js
+++ webpack.prd.config.js
@@ -1,4 +1,3 @@
-
 var webpack = require('webpack');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
 var merge = require('webpack-merge');
@@ -6,17 +5,17 @@ var webpackBaseConfig = require('./webpack.config');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 
-module.exports = merge(webpackBaseConfig,{
+module.exports = merge(webpackBaseConfig, {
 
-    output:{
-        publicPath:'./dist/',
-        filename:'[name].[hash:7].js',
+    output: {
+        publicPath: './dist/',
+        filename: '[name].[hash:7].js',
     },
-    plugins:[
+    plugins: [
         //定义当前的node环境为生产环境
         new webpack.DefinePlugin({
-            'process.env':{
-                NODE_ENV:'"production"'
+            'process.env': {
+                NODE_ENV: '"production"'
             }
         }),
         //压缩js
@@ -30,9 +29,9 @@ module.exports = merge(webpackBaseConfig,{
         //提取模版，并保存入口html文件
         //index_prd.html会根据index.ejs模版自动生成
         new HtmlwebpackPlugin({
-            filename:'../index_prd.html',
-            template:'./index.ejs',
-            inject:false,
+            filename: '../index.html',
+            template: './index.ejs',
+            inject: false,
         }),
     ],
 });
